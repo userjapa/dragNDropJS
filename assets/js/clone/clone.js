f441d05fea9d0f99dec0179be8690bfa7f62b4e3
usuarioJapa <willian.hsimabukuro@gmail.com>
2017-07-17 18:49:30 -0300

    fixed edit

--- assets/js/clone/clone.js
+++ assets/js/clone/clone.js
@@ -8,12 +8,12 @@ let get = () => {
 let set = (el) => {
     if (new RegExp('drag-copy').test(el.parentNode.className)) {
         toDrop = el.cloneNode(true);
-        console.log('Cloned '+ toDrop.className + '---------')
+        console.log('Cloned ' + toDrop.className + '---------')
     } else {
         toDrop = el;
-        console.log('Got '+ toDrop.className + '---------')
+        console.log('Got ' + toDrop.className + '---------')
     }
-    allowEdit(toDrop)
+    el.contentEditable = true;
 }
 
 let checkEl = (el) => {
@@ -24,19 +24,10 @@ let checkEl = (el) => {
     }
 }
 
-let allowEdit = (el) => {
-    var tags = ['P','PRE','H1','H2','H3','H4','H5','H6','SPAN','STRONG']
-    var itens = el.childNodes
-    for (e of itens) {
-        if (tags.includes(e.tagName))
-            e.contentEditable = true
-    }
-}
- 
 const drop = {
-    get : get,
-    set : set,
-    checkEl : checkEl
+    get: get,
+    set: set,
+    checkEl: checkEl
 }
 
 module.exports = drop
--- dragndrop.js
+++ dragndrop.js
@@ -81,12 +81,12 @@ let get = () => {
 let set = (el) => {
     if (new RegExp('drag-copy').test(el.parentNode.className)) {
         toDrop = el.cloneNode(true);
-        console.log('Cloned '+ toDrop.className + '---------')
+        console.log('Cloned ' + toDrop.className + '---------')
     } else {
         toDrop = el;
-        console.log('Got '+ toDrop.className + '---------')
+        console.log('Got ' + toDrop.className + '---------')
     }
-    allowEdit(toDrop)
+    el.contentEditable = true;
 }
 
 let checkEl = (el) => {
@@ -97,19 +97,10 @@ let checkEl = (el) => {
     }
 }
 
-let allowEdit = (el) => {
-    var tags = ['P','PRE','H1','H2','H3','H4','H5','H6','SPAN','STRONG']
-    var itens = el.childNodes
-    for (e of itens) {
-        if (tags.includes(e.tagName))
-            e.contentEditable = true
-    }
-}
- 
 const drop = {
-    get : get,
-    set : set,
-    checkEl : checkEl
+    get: get,
+    set: set,
+    checkEl: checkEl
 }
 
 module.exports = drop
